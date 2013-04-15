f6d16f0ca235747a11f4667f1b63a8ab43f217c5
Tyler Compton <xaviosx@gmail.com>
2013-04-15 08:39:55 -0700

    Add TestSubject class.

--- src/com/teamtyro/src/DataAnalyzer.java
+++ src/com/teamtyro/src/DataAnalyzer.java
@@ -4,4 +4,8 @@ public class DataAnalyzer {
 	public static void main(String[] args) {
 		System.out.printf("DataAnalyzer V 0.0.1\n");
 	}
+	
+	private void render() {
+		
+	}
 }
---
+++ src/com/teamtyro/src/TestSubject.java
@@ -0,0 +1,15 @@
+package com.teamtyro.src;
+
+public class TestSubject {
+	int actions[];
+	public TestSubject() {
+		
+	}
+	
+	public TestSubject(int[] a) {
+		actions = new int[a.length];
+		for(int i=0; i<a.length; i++) {
+			a[i] = actions[i];
+		}
+	}
+}
